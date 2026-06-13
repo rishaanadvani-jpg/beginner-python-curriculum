1809716a0fb75eacac213eb27f60984d3d7155e2
rishaanadvani-jpg <rishaanadvani@gmail.com>
2026-06-13 11:55:31 -0700

    lesson 1 homework

--- lesson1/homework/problems.py
+++ lesson1/homework/problems.py
@@ -1,24 +1,33 @@
 # Problem 1
 # Create a variable for your lucky number and print it.
-
-
+lucky_number = 13
+print(lucky_number)
 
 # Problem 2
 # Create a variable for the number of hours you sleep and print it.
-
+hours_of_sleep = 9.5
+print(hours_of_sleep)
 
 
 # Problem 3
 # Create a variable for your favorite fruit and print it in a full sentence.
-
-
+favorite_fruit = "mango"
+print(f"My favorite fruit is {favorite_fruit}.")
 
 # Problem 4
 # Create two variables: one for your city and one for your country.
 # Print them on 2 separate lines.
-
-
+city = "Seattle"
+country = "USA"
+print(city)
+print(country)
 
 # Problem 5
 # Create 3 variables: your pet's name, its age, and its type.
 # Print them on the same line in a full sentence.
+pet_name = "rishaan"
+pet_age = 3
+pet_type = "dog"
+print(f"My {pet_type} {pet_name} is {pet_age} years old.")
+
+
